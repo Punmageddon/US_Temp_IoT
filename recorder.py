972be4b8b20199d61b4c78f691d3ff8f31ece4ba
Ruben <horn.ruben@yahoo.de>
2020-02-25 21:52:19 +0100

    Auto reconnect, resynchronize rows, refactoring

--- recorder.py
+++ recorder.py
@@ -6,6 +6,7 @@ import argparse
 import os.path
 import time
 from datetime import datetime
+import calendar
 
 try:
     import paho.mqtt.client as mqtt
@@ -24,8 +25,10 @@ parser.add_argument("-u", "--user", default=None,
                     help="Username for the MQTT broker")
 parser.add_argument("-pw", "--password", default=None,
                     help="Password for the MQTT broker")
+parser.add_argument("-t", "--value-timeout", default=5, type=int,
+                    help="Maximum time in seconds between two values from different columns to be grouped into the same row")
 parser.add_argument("-d", "--daily-file", action="store_true",
-                    help="Store records in separate files for each day (date will be appended to filename)")
+                    help="Store measurements in separate files for each day (date will be appended to filename)")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="Print all incoming messages")
 
@@ -35,46 +38,68 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-temperatureGroundTruthValues = []
-temperatureValues = []
-humidityValues = []
+temperature_ground_truth = None
+temperature = None
+humidity = None
+last_received_time = 0
 
 
 def on_connect(client, userdata, flags, rc):
     print("Connected!")
     print("Subscribing to sensor at {}/...".format(args.sensor))
-    mqttClient.subscribe(args.sensor + "/temperatureGroundTruth")
-    mqttClient.subscribe(args.sensor + "/temperature")
-    mqttClient.subscribe(args.sensor + "/humidity")
+    mqtt_client.subscribe(args.sensor + "/temperatureGroundTruth")
+    mqtt_client.subscribe(args.sensor + "/temperature")
+    mqtt_client.subscribe(args.sensor + "/humidity")
 
 
 def on_disconnect(client, userdata, rc):
     print("Disconnected!")
-    exit(1)
+
+
+def timeout_values():
+    global temperature_ground_truth
+    global temperature
+    global humidity
+    global last_received_time
+
+    if time.time() - last_received_time > args.value_timeout:
+        if args.verbose:
+            print("Synchronized! Begining new row.")
+        temperature_ground_truth = None
+        temperature = None
+        humidity = None
+    last_received_time = time.time()
 
 
 def on_message(client, userdata, msg):
+    global temperature_ground_truth
+    global temperature
+    global humidity
+
+    timeout_values()
     if args.verbose:
         print(msg.topic + " " + str(msg.payload))
     value = float(msg.payload)
     if msg.topic.endswith("/temperatureGroundTruth"):
-        temperatureGroundTruthValues.append(value)
+        temperature_ground_truth = value
     elif msg.topic.endswith("/temperature"):
-        temperatureValues.append(value)
+        temperature = value
     else:  # "/humidity"
-        humidityValues.append(value)
-    if len(temperatureGroundTruthValues) > 0 and len(temperatureValues) > 0 and len(humidityValues) > 0:
-        record = [temperatureGroundTruthValues.pop(
-            0), temperatureValues.pop(0), humidityValues.pop(0)]
-        store_record(record)
+        humidity = value
+    if temperature_ground_truth != None and temperature != None and humidity != None:
+        store_row([temperature_ground_truth, temperature, humidity])
+        temperature_ground_truth = None
+        temperature = None
+        humidity = None
 
 
-def store_record(record):
+def store_row(row):
+    utc_timestamp = calendar.timegm(datetime.utcnow().utctimetuple())
     if args.daily_file:
-        create_records_file_if_not_exists()
+        create_current_file_if_not_exists()
     with open(get_filename(), "a") as file:
         file.write("{0},{1},{2},{3}\n".format(
-            int(time.time()), record[0], record[1], record[2]))
+            utc_timestamp, row[0], row[1], row[2]))
 
 
 def get_filename():
@@ -84,30 +109,30 @@ def get_filename():
         return args.out
 
 
-def create_records_file_if_not_exists():
+def create_current_file_if_not_exists():
     if not os.path.isfile(get_filename()):
         with open(get_filename(), "w+") as file:
             file.write(
-                "timestamp,temperatureGroundTruth,temperature,humidity\n")
+                "timestamp,temperature_ground_truth,temperature,humidity\n")
 
 
 if not args.daily_file:
-    create_records_file_if_not_exists()
+    create_current_file_if_not_exists()
 
-mqttClient = mqtt.Client()
-mqttClient.on_connect = on_connect
-mqttClient.on_message = on_message
-mqttClient.on_disconnect = on_disconnect
+mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+mqtt_client.on_disconnect = on_disconnect
 
 print("Connecting to {0}:{1}...".format(args.broker, args.port))
-mqttClient.username_pw_set(args.user, args.password)
+mqtt_client.username_pw_set(args.user, args.password)
 try:
-    mqttClient.connect(args.broker, args.port)
+    mqtt_client.connect(args.broker, args.port)
 except:
     print("Error! Could not connect to {0} on Port {1}".format(
         args.broker, args.port), file=sys.stderr)
     exit(1)
 try:
-    mqttClient.loop_forever()
+    mqtt_client.loop_forever()
 except KeyboardInterrupt:
     pass
